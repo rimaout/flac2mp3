39b6e5fdfe20832e0532160d5d35a813d6881529
jaxkeeper <116072651+Jaxkeeper@users.noreply.github.com>
2024-02-25 16:45:08 +0100

    Multithreading and optional output directory argument

---
+++ custom_functions.py
@@ -0,0 +1,91 @@
+
+# Command line arguments
+import argparse, os
+from mutagen.flac import FLAC   # Metadata
+from pydub import AudioSegment  # Audio processing
+
+def collect_args():
+    parser = argparse.ArgumentParser(description='This script converts .flac files to .mp3 files, It provides functionality to convert a single FLAC file or all FLAC files within a directory and its subdirectories.')
+    parser.add_argument('input_path', type=str, nargs='?', default=os.getcwd(), help='The path to the directory containing the files to process. Default is the current directory.')
+    parser.add_argument('--num_threads', '-t', type=int, default=1, help='The number of threads to use. Default is 1.')
+    parser.add_argument('--output_path', '-o', type=str, default=None, help='The path to the directory where the output files will be saved. Default is the same directory as the input file.')
+
+    args = parser.parse_args()
+    
+    #Is a path
+    if not os.path.exists(args.input_path):
+        parser.error("The file_path %s does not exist!" % args.input_path)
+
+    # check if the number of threads is an integer
+    if not isinstance(args.num_threads, int):
+        parser.error("The number of threads must be an integer")
+        
+    if args.num_threads < 1:
+        parser.error("The number of threads must be at least 1")
+    
+    if args.output_path is None: 
+        # if the output_path is not specified
+
+        if os.path.isfile(args.input_path): 
+            # if the input_path is a file, the output_path is the directory of the input_path
+            args.output_path = os.path.dirname(args.input_path) 
+        
+        else: 
+            # if the input_path is a directory, the output_path is the input_path + "_converted"
+            args.output_path = args.input_path + "_converted" 
+    
+    return args.input_path, args.num_threads, args.output_path
+  
+    
+def dir_scan(input_dir):
+    '''
+    Funcion that scans a given directory and return the number of files, and a list of the file_paths to the files
+    '''
+
+    flac_count = 0
+    flac_list = []
+    file_count = 0
+    file_list = []
+
+    for elem in os.listdir(input_dir):
+        elem_path = os.path.join(input_dir, elem)
+
+        # if file is a directory, call the function recursively
+        if os.path.isdir(elem_path):
+            recursive_flac_count, recursive_flac_list, recursive_file_count, recursive_file_list = dir_scan(elem_path)
+            
+            flac_count += recursive_flac_count
+            flac_list.extend(recursive_flac_list)
+            
+            file_count += recursive_file_count
+            file_list.extend(recursive_file_list)
+            
+        elif elem.endswith(".flac"):
+            flac_count += 1
+            flac_list.append(elem_path)
+        
+        else:
+            file_count += 1
+            file_list.append(elem_path)
+            
+    return flac_count, flac_list, file_count, file_list
+
+
+def replace_extension(file, new_extension):
+    # replace file extension
+    return os.path.splitext(file)[0] + new_extension
+
+
+def get_metadata(file):
+    # get metadata from flac file
+
+    metadata = FLAC(file)
+    tags = {}
+    for data in metadata:
+        tags[data] = metadata[data][0]
+    return tags
+
+def flac_mp3(input_file_path, output_file_path):
+    # convert flac file to mp3
+    flac_audio = AudioSegment.from_file(input_file_path, format="flac")
+    flac_audio.export(output_file_path, format="mp3", bitrate="320k", tags=get_metadata(input_file_path))
--- main.py
+++ main.py
@@ -1,146 +1,88 @@
-#script description: 
-# This script converts flac files to mp3 files. It can be used to convert a single file or a directory containing flac files. 
+import custom_functions as cf
+import os, shutil, time, sys
 
-# Audio conversion
-from pydub import AudioSegment  # Audio conversion 
-from mutagen.flac import FLAC   # Metadata
-
-# Progress bar
-from alive_progress import alive_bar
-import time
-
-#File handling
-import os, shutil
-
-# Command line arguments
-import argparse
-
-def collect_args():
-    parser = argparse.ArgumentParser(description='This script converts .flac files to .mp3 files, It provides functionality to convert a single FLAC file or all FLAC files within a directory and its subdirectories.')
-    parser.add_argument('filepath', type=str, nargs='?', default=os.getcwd(), help='The path to the directory containing the files to process. Default is the current directory.')
-    parser.add_argument('--num_threads', type=int, default=1, help='The number of threads to use. Default is 1.')
-
-    args = parser.parse_args()
-    
-    #Is a path
-    if not os.path.exists(args.filepath):
-        parser.error("The file_path %s does not exist!" % args.filepath)
-
-    # check if the number of threads is an integer
-    if not isinstance(args.num_threads, int):
-        parser.error("The number of threads must be an integer")
-        
-    if args.num_threads < 1:
-        parser.error("The number of threads must be at least 1")
-    
-    return args.filepath, args.num_threads
-
-def count_flac(input_path):
-    # if input is a single file, return 1
-    if os.path.isfile(input_path):
-        if input_path.endswith(".flac"):
-            return 1
-        else:
-            return 0
-    
-    # if input is a directory, count flac files recursively
-    flac_count = 0
-    for elem in os.listdir(input_path):
-        elem_path = os.path.join(input_path, elem)
-
-        # if file is a flac file, convert it to mp3
-        if elem.endswith(".flac"):
-            flac_count += 1
-
-        # if file is a directory, call the function recursively
-        elif os.path.isdir(elem_path):
-            flac_count += count_flac(elem_path)
-            
-    return flac_count
-
-def replace_extension(file, new_extension):
-    # replace file extension
-    return os.path.splitext(file)[0] + new_extension
+from concurrent.futures import ThreadPoolExecutor # Multithreading
+from alive_progress import alive_bar # Progress bar
 
 
-def single_file_conversion(file):
+def single_file_conversion(file_path, output_dir):
     # convert single file to mp3
     
-    if file.endswith(".flac"):  # if file is a flac file, convert it to mp3
-        flac_mp3(file, replace_extension(file, ".mp3"))
-        count[0] += 1
+    if file_path.endswith(".flac"):  # if file is a flac file, convert it to mp3
+        output_path = output_dir + "/" + os.path.basename(file_path).replace(".flac", ".mp3")
+        cf.flac_mp3(file_path, output_path)
+        flac_count, file_count = [1], [0]
         bar()
 
     else:
         print("File is not a flac file")
+        sys.exit(1)
+
+    return flac_count, file_count
+
+def flac_conversion(input_file_path, output_file_path, flac_count):
+    # convert flac file to mp3
+    cf.flac_mp3(input_file_path, output_file_path)
+    flac_count[0] += 1
+    bar()
 
 
-def directory_flac_to_mp3(input_dir, output_dir, flac_count):
-    # recursive function to convert flac files to mp3, and subdirectories
-   
-    for file in os.listdir(input_dir):
-        input_file_path = os.path.join(input_dir, file)
-        output_file_path = os.path.join(output_dir, file)
-
-        # if file is a flac file, convert it to mp3
-        if file.endswith(".flac"):
-            flac_mp3(input_file_path, replace_extension(output_file_path, ".mp3"))
-            flac_count[0] += 1
-            bar()
-
-        # if file is a directory, call the function recursively
-        elif os.path.isdir(input_file_path):
-            os.makedirs(output_file_path, exist_ok=True)
-            directory_flac_to_mp3(input_file_path, output_file_path, flac_count)
-
-        # if file is not flac, copy file to output directory
-        else:
-            shutil.copy(input_file_path, output_file_path)
+def file_copy(input_file_path, output_file_path, file_count):
+    # copy file to output directory
+    shutil.copy(input_file_path, output_file_path)
+    file_count[0] += 1
+    bar()
 
 
-def get_metadata(file):
-    # get metadata from flac file
-
-    metadata = FLAC(file)
-    tags = {}
-    for data in metadata:
-        tags[data] = metadata[data][0]
-    return tags
-
-
-def flac_mp3(file_in, file_out):
-    # convert flac to mp3
-
-    flac_audio = AudioSegment.from_file(file_in, format="flac")
-    flac_audio.export(file_out, format="mp3", bitrate="320k", tags=get_metadata(file_in))
+def flac_thread(flac_list, input_dir, output_dir, flac_count, theads_count):
+        with ThreadPoolExecutor(max_workers=theads_count) as executor:
+            futures = []
+            for file_path in flac_list:
+            
+                output_file_path = file_path.replace(input_dir, output_dir).replace(".flac", ".mp3")
+                os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+                futures.append(executor.submit(flac_conversion, file_path, output_file_path, flac_count))
+            
+            for future in futures:
+                future.result()
 
 
-def main(input_dir, start_time, flac_count):
-    
-    # if input is a single file, convert it to mp3
-    if os.path.isfile(input_dir):
-        single_file_conversion(input_dir)
+def file_thread(file_list, input_dir, output_dir, file_count, theads_count):
+        with ThreadPoolExecutor(max_workers=theads_count) as executor:
+            futures = []
+            for file_path in file_list:
+                
+                output_file_path = file_path.replace(input_dir, output_dir)
+                os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+                futures.append(executor.submit(file_copy, file_path, output_file_path, file_count))
 
-    # input is a directory
-    else:
-        # create output directory
-        output_dir = f"{input_dir}_converted" 
-        os.makedirs(output_dir, exist_ok=True)
+            for future in futures:
+                future.result()
 
-        # convert flac files to mp3 recursively
-        directory_flac_to_mp3(input_dir, output_dir, flac_count)
 
 if __name__ == "__main__":
-    input_path, thread_count = collect_args()
-
+    input_path, thread_count, output_path = cf.collect_args()
+    input_path = os.path.abspath(input_path)
+    output_path = os.path.abspath(output_path)
     start_time = time.time()
-    flac_count = [0]
-    flac_total = count_flac(input_path)
 
-    with alive_bar(flac_total, title="Processing:", bar="notes", spinner_length=5, receipt=False, elapsed=False, stats=None) as bar:
-        main(input_path, start_time, flac_count)
+    if os.path.isfile(input_path):
+        with alive_bar(title="Processing single file:", bar=False, spinner_length=5, receipt=False, elapsed=False, stats=None, monitor=False) as bar:
+            flac_count, file_count = single_file_conversion(input_path, output_path)
+       
+    else:
+        flac_count, file_count = [0], [0]
+        flac_total, flac_list, file_total, file_list = cf.dir_scan(input_path)
+ 
+        os.makedirs(output_path, exist_ok=True)
+
+        with alive_bar(flac_total, title="Processing flac files:", bar="notes", spinner_length=5, receipt=False, elapsed=False, stats=None) as bar:
+            flac_thread(flac_list, input_path, output_path, flac_count, thread_count)
+
+        with alive_bar(file_total, title="Coping other files:", bar="smooth", spinner_length=5, receipt=False, elapsed=False, stats=None) as bar:
+            file_thread(file_list, input_path, output_path, file_count, thread_count)
 
     total_time = time.time() - start_time
     minutes = int(total_time // 60)
     seconds = int(round(total_time % 60))
-    print(f"Done! Converted {flac_count[0]} files in {minutes}:{seconds} minutes.")
+    print(f"Done! Converted {flac_count[0]} flac files and copied {file_count[0]} files in {minutes}.{seconds} minutes.")
